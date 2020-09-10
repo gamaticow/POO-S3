032c96107d8b4aab9618d71b9be42f78e12b2f31
Corentin <corentincouteaux@gmail.com>
2020-09-10 09:22:39 +0200

    GuitarV1

---
+++ Design Patterns exercices/src/structure/facade/TestHomeCinema.java
@@ -0,0 +1,35 @@
+package structure.facade;
+
+public class TestHomeCinema {
+	
+	public static void main(String[] args) {
+		// instanciation des composants
+		Amplificateur amp = new Amplificateur();
+		Tuner tuner = new Tuner();
+		LecteurDvd dvd = new LecteurDvd();
+		LecteurCd cd = new LecteurCd();
+		Projecteur projecteur = new Projecteur();
+		Ecran ecran = new Ecran();
+		Lumieres lumieres = new Lumieres();
+		MachineAPopcorn machineAPopCorn = new MachineAPopcorn();
+		
+		machineAPopCorn.marche();
+		machineAPopCorn.eclater();
+		lumieres.attenuer(10);
+		ecran.baisser();
+		projecteur.marche();
+		projecteur.setEntree(dvd);
+		projecteur.modeGrandEcran();
+		amp.marche();
+		amp.setDvd(dvd);
+		amp.setSonSurround();
+		amp.setVolume(5);
+		dvd.marche();
+		dvd.jouer("The Joker");
+		
+		//FacadeHomeCinema homeCinema = null;
+
+		//homeCinema.regarderFilm("Edge of tomorrow");
+		//homeCinema.arreterFilm(); 
+	}
+}
